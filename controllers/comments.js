a9d33444b6b063ecdec89c4d010bb61b8c0e7dcc
jsm-txt <hose153@gmail.com>
2021-04-21 16:33:27 -0500

    Users can comment

---
+++ controllers/comments.js
@@ -0,0 +1,15 @@
+module.exports = function(app) {
+
+};
+const Comment = require('../models/comment');
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+  content: { type: String, required: true }
+},
+  {timestamps: {createdAt: 'created_at'}}
+);
+
+module.exports = mongoose.model("Comment", CommentSchema);
+
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const expressValidator = require('express-validator');
 require('./data/reddit-db');
 // server 
 const app = express()
+require('./controllers/comments.js')(app);
 //middle ware
 app.use(express.json());
 app.use(express.urlencoded({
@@ -24,4 +25,31 @@ app.get('/', (req, res) => {
 app.get('/posts/new', (req,res) => {
     res.render('posts-new')
 })
+const Post = require('../models/post');
+const Comment = require('../models/comment');
+
+// CREATE Comment
+app.post("/posts/:postId/comments", function(req, res) {
+  // INSTANTIATE INSTANCE OF MODEL
+  const comment = new Comment(req.body);
+
+  // SAVE INSTANCE OF Comment MODEL TO DB
+  comment
+    .save()
+    .then(comment => {
+      return Post.findById(req.params.postId);
+    })
+    .then(post => {
+      post.comments.unshift(comment);
+      return post.save();
+    })
+    .then(post => {
+      res.redirect(`/`);
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
 app.listen(3000)
+module.exports = app;

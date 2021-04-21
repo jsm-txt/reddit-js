df6a43df4878a2652887b7f663936f8ac7fc46e8
jsm-txt <hose153@gmail.com>
2021-04-21 15:47:23 -0500

    Users can see individual posts

--- controllers/posts.js
+++ controllers/posts.js
@@ -7,30 +7,42 @@ const PostSchema = new Schema({
   summary: { type: String, required: true }
 });
 module.exports = mongoose.model("Post", PostSchema);
+const Post = require('../models/post');
 
 
-module.exports = app => {
-    // CREATE
-    app.get('/', (req, res) => {
-        Post.find({}).lean()
-          .then(posts => {
-            res.render('posts-index', { posts });
-          })
-          .catch(err => {
-            console.log(err.message);
-          })
+module.exports = (app) => {
+
+  // CREATE
+  app.post('/posts/new', (req, res) => {
+    // INSTANTIATE INSTANCE OF POST MODEL
+    const post = new Post(req.body);
+
+    // SAVE INSTANCE OF POST MODEL TO DB
+    post.save((err, post) => {
+      // REDIRECT TO THE ROOT
+      return res.redirect(`/`);
+    })
+  });
+
+  app.get('/', (req, res) => {
+    Post.find({}).lean()
+      .then(posts => {
+        res.render('posts-index', { posts });
       })
-
-      app.get("/posts/:id", function(req, res) {
-        // LOOK UP THE POST
-        Post.findById(req.params.id).lean()
-          .then(post => {
-            res.render("posts-show", { post });
-          })
-          .catch(err => {
-            console.log(err.message);
-          });
+      .catch(err => {
+        console.log(err.message);
+      })
+  })
+  app.get("/posts/:id", function(req, res) {
+    // LOOK UP THE POST
+    Post.findById(req.params.id).lean()
+      .then(post => {
+        res.render("posts-show", { post });
+      })
+      .catch(err => {
+        console.log(err.message);
       });
-  };
+  });
 
+};
   
--- server.js
+++ server.js
@@ -1,43 +1,27 @@
+// imports
 const express = require('express')
-const app = express()
-const port = 3000
-
+const handlebars = require('handlebars')
 const bodyParser = require('body-parser');
 const expressValidator = require('express-validator');
-
-const Post = require('../models/post');
-require('./controllers/posts.js')(app);
 require('./data/reddit-db');
-
-// Use Body Parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-// Add after body parser initialization!
+// server 
+const app = express()
+//middle ware
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(expressValidator());
-
+const exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+// controllers 
+require('./controllers/posts.js')(app)
+// routes
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.render('head')
 })
-const Post = require('../models/post');
-
-module.exports = (app) => {
-
-  // CREATE
-  app.post('/posts/new', (req, res) => {
-    // INSTANTIATE INSTANCE OF POST MODEL
-    const post = new Post(req.body);
-
-    // SAVE INSTANCE OF POST MODEL TO DB
-    post.save((err, post) => {
-      // REDIRECT TO THE ROOT
-      return res.redirect(`/`);
-    })
-  });
-
-};
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+app.get('/posts/new', (req,res) => {
+    res.render('posts-new')
 })
-
+app.listen(3000)
